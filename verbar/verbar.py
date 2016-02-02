8248de438f0fa55aba7a57cf72e97a63d5901238
Sutee Dee <thaiyoshi@gmail.com>
2016-02-02 17:17:59 +0700

    defines irregularity settings and tests for level of irregularity

--- verbar/verbar.py
+++ verbar/verbar.py
@@ -1,11 +1,20 @@
 __author__ = 'sutee'
 from conjugation import *
 import random
+from enum import Enum
 
-verbs = ['hablar', 'ir']
+from collections import namedtuple
+
+verbs = ['hablar', 'ir', 'dormir']
 #maps pronouns and tenses to conventions used for keys in conjugation library
 pronouns = {'yo': '1', 'tu': '2', 'el': '3', 'nosotros': '4', 'ellos': '6' }
 tenses = {'present': 'pre'}
+
+
+class IrregularityChoice(Enum):
+    all = 1 #
+    none = 2
+    bycase =3
 
 def _main():
     generate_conjugation()
@@ -26,27 +35,44 @@ def choose_tense():
 def generate_key(pronoun, tense):
     return pronoun[1]+tense[1]
 
-def is_irregular(verb, pronoun, tense, blanket_filter=True):
+def is_irregular(verb, pronoun, tense, filter=IrregularityChoice.all):
     #simple filter than eliminates verbs that are irregular in any tense
-    if blanket_filter:
+    if filter == IrregularityChoice.none:
         return is_irregular_ever(verb)
+    elif filter== IrregularityChoice.bycase:
+        return is_irregular_here(verb, pronoun, tense)
+    else:
+        return False
 
 def is_irregular_ever(verb):
     return verb in irregular_verbs.keys()
 
-def is_irregular_ever(verb):
-    pass
+def is_irregular_here(verb, pronoun, tense):
+    key = generate_key(pronoun, tense)
+    if irregular_verbs.has_key(verb) and irregular_verbs[verb].has_key(key):
+        return True
+    else:
+        return False
 
 def generate_conjugation():
-    verb = choose_verb()
+    irregularity_setting = IrregularityChoice.bycase
     pronoun = choose_pronoun()
     tense = choose_tense()
+    verb = choose_verb()
+
+    if irregularity_setting is not IrregularityChoice.all:
+        irregular = is_irregular(verb, pronoun, tense, irregularity_setting)
+        if irregular: #if irregular, try again
+            return generate_conjugation()
+
     key = generate_key(pronoun, tense)
+    print key
     conjugation = conjugate(verb)
     question = verb, pronoun[0], tense[0]
     answer = conjugation[key]
     print "Question:", question
     print "Answer:", answer
+    return (question, answer)
 
 if __name__ == "__main__":
     _main()
@@ -57,4 +83,4 @@ if __name__ == "__main__":
 # print len(irregular_verbs.items()[11][1].keys())
 # print len(irregular_verbs.items()[21][1].keys())
 # print irregular_verbs.items()[21]
-# print irregular_verbs.values()
+# print irregular_verbs['dormir'].has_key('4pre')
