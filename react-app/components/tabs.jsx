c5054143f1b374dba8081e41d627e73da0aa94c2
Sutee Dee <thaiyoshi@gmail.com>
2016-02-11 22:38:32 +0700

    adds specific sections

--- react-app/components/tabs.jsx
+++ react-app/components/tabs.jsx
@@ -10,9 +10,15 @@ var Tabs = React.createClass({
         return (
             <div>
                 <ReactBootstrap.Tabs defaultActiveKey={1}>
-                    <ReactBootstrap.Tab eventKey={1} title="Tab 1">Tab 1 content</ReactBootstrap.Tab>
-                    <ReactBootstrap.Tab eventKey={2} title="Tab 2">Tab 2 content</ReactBootstrap.Tab>
-                    <ReactBootstrap.Tab eventKey={3} title="Tab 3">Tab 3 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={1} title="Planning">
+                        Tab 1 content
+                    </ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={2} title="Arriving">Tab 2 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={3} title="Eating">Tab 3 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={4} title="Exploring">Tab 4 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={5} title="Interacting">Tab 5 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={6} title="Shopping">Tab 6 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={7} title="Caution">Tab 7 content</ReactBootstrap.Tab>
                 </ReactBootstrap.Tabs>
             </div>);
     }
