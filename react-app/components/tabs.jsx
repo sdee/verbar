43870d2e7b4fb12c7a339cfa1e6d0bf371b9f43a
Sutee Dee <thaiyoshi@gmail.com>
2016-02-15 20:14:00 +0700

    tweak ui

--- react-app/components/tabs.jsx
+++ react-app/components/tabs.jsx
@@ -13,9 +13,53 @@ var Tabs = React.createClass({
                     <ReactBootstrap.Tab eventKey={1} title="Planning">
                         Tab 1 content
                     </ReactBootstrap.Tab>
-                    <ReactBootstrap.Tab eventKey={2} title="Arriving">Tab 2 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={2} title="Arriving">
+                        Tab 2 content
+                    </ReactBootstrap.Tab>
                     <ReactBootstrap.Tab eventKey={3} title="Eating">Tab 3 content</ReactBootstrap.Tab>
-                    <ReactBootstrap.Tab eventKey={4} title="Exploring">Tab 4 content</ReactBootstrap.Tab>
+                    <ReactBootstrap.Tab eventKey={4} title="Exploring">
+                        <ReactBootstrap.Table responsive>
+                            <thead>
+                                <tr>
+                                    <th>What</th>
+                                    <th>Where</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Iconic Landmark</td>
+                                    <td>Golden Gate Bridge</td>
+                                </tr>
+                                 <tr>
+                                    <td>New Landmark</td>
+                                    <td>Bay bridge</td>
+                                </tr>
+                                <tr>
+                                    <td>Postcard Photo</td>
+                                    <td>Golden Gate bridge from Baker Beach</td>
+                                </tr>
+                                <tr>
+                                    <td>Most Hipster neighborhood</td>
+                                    <td>The Mission</td>
+                                </tr>
+
+                                <tr>
+                                    <td>Best Stroll</td>
+                                    <td>Along the Embarcadero</td>
+                                </tr>
+                                <tr>
+                                    <td>Best View</td>
+                                    <td>Twin Peaks</td>
+                                </tr>
+                                <tr>
+                                    <td>Place to Escape</td>
+                                    <td>Napa Valley, Highway 1</td>
+                                </tr>
+
+                            </tbody>
+                        </ReactBootstrap.Table>
+
+                    </ReactBootstrap.Tab>
                     <ReactBootstrap.Tab eventKey={5} title="Interacting">Tab 5 content</ReactBootstrap.Tab>
                     <ReactBootstrap.Tab eventKey={6} title="Shopping">Tab 6 content</ReactBootstrap.Tab>
                     <ReactBootstrap.Tab eventKey={7} title="Caution">Tab 7 content</ReactBootstrap.Tab>
@@ -23,5 +67,4 @@ var Tabs = React.createClass({
             </div>);
     }
 });
-
 module.exports = Tabs
