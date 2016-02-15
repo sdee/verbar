f0ef285385536a47e07a5022684176a379b81bcb
Sutee Dee <thaiyoshi@gmail.com>
2016-02-15 21:52:39 +0700

    prod tweaks

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    "./react-app/app.jsx",
+    "./react-app/app",
   ],
   output: {
     path: path.join(__dirname, 'public'),
