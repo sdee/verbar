764ad50a01841869f9b21aa0ce132d83ba87212f
Sutee Dee <thaiyoshi@gmail.com>
2016-02-16 01:37:20 +0700

    prepare for deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 var app = express();
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
-var static_path = path.join(process.env.PWD, 'public');
+var static_path = path.join(__dirname, 'public');
 
 app.use(express.static(static_path))
   .get('/', function (req, res) {
